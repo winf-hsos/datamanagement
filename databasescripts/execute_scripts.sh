8b0f3e0a45844296a04c17b8f6eddff63ee7ad06
Nicolas <nicolas.meseth@googlemail.com>
2017-03-06 01:10:32 +0000

    Changed paths to relative

--- databasescripts/execute_scripts.sh
+++ databasescripts/execute_scripts.sh
@@ -3,10 +3,10 @@ sqlplus -s system/manager@//localhost:1521/XE << EOF
 whenever sqlerror exit sql.sqlcode;
 set heading off
 
-@/home/vagrant/vagrant-ubuntu-oracle-xe/database/create_user.sql
-@/home/vagrant/vagrant-ubuntu-oracle-xe/database/drop_tables.sql
-@/home/vagrant/vagrant-ubuntu-oracle-xe/database/create_tables.sql
-@/home/vagrant/vagrant-ubuntu-oracle-xe/database/insert_product.sql
-@/home/vagrant/vagrant-ubuntu-oracle-xe/database/insert_product_category.sql
+@create_user.sql
+@drop_tables.sql
+@create_tables.sql
+@insert_product.sql
+@insert_product_category.sql
 exit;
 EOF
