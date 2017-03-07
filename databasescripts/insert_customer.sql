001b32c0ffde55716686f05c71ce2ef79ac8bbd9
Nicolas <nicolas.meseth@googlemail.com>
2017-03-07 07:56:29 +0000

    Test new scripts

--- databasescripts/insert_customer.sql
+++ databasescripts/insert_customer.sql
@@ -1,1 +1,1 @@
-INSERT INTO student.customer (id, name, address_city, address_plz, address_street, address_no, type, rating) VALUES (1, 'Hopsession Craftbeerstore', 'Osnabrück', 49074, 'Heger Straße', '13', 'shop', 'B');
+INSERT INTO student.customer (id, name, city, plz, street, no, type, rating) VALUES (1, 'Hopsession Craftbeerstore', 'Osnabrück', 49074, 'Heger Straße', '13', 'shop', 'B');
