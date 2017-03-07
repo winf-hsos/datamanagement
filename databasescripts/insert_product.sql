da8772904ba78fd00b06ae566a1c2993c551064b
Nicolas <nicolas.meseth@googlemail.com>
2017-03-07 07:22:34 +0000

    Test new scripts

--- databasescripts/insert_product.sql
+++ databasescripts/insert_product.sql
@@ -1,29 +1,18 @@
--- 3 = Softdrinks
 INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (1, 'Coca Cola', 1, 12, 11.99, 0, 0, null);
-
-
-/*
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(1, 'Sinalco Cola', 'Kiste mit 24 Flaschen a 0,5 l', 3);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(2, 'Coca Cola', 'Kiste mit 24 Flaschen a 0,33 l', 3);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(3, 'Fanta', 'Kiste mit 24 Flaschen a 0,33 l', 3);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(4, 'Gerolsteiner Mineralwasser', 'Kiste mit 12 Flaschen a 0,75 l', 3);
-
--- 4 = Warme Getränke
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(5, 'Dahlmayr Prodomo Kaffee (100% Arabica)', '500 g Packung gemahlen', 4);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(6, 'Dallmayr Kaffee Crema mild und fein Kaffeebohnen', '500 g Packung gemahlen', 4);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(7, 'Nestlé Nesquik kakaohaltiges Getränkepulver', '900 g Dose', 4);
-
--- 9 = Biere - Pilsener
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(100, 'Franziskaner Weizen', '0,5 L Flasche', 9);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(101, 'Erdinger Weizen', '0,5 L Flasche', 9);
-
--- 11 = Biere - IPA
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(201, 'Propeller Aufwind IPA', '0,33 L Flasche', 11);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(202, 'CREW Republic Drunken Sailor IPA', '0,33 L Flasche', 11);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(203, 'CREW Republic 7:45 Esacalation Double IPA', '0,33 L Flasche', 11);
-
--- 12 = Biere - Kölsch
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(300, 'Hess Claritas Kölsch', '0,66 L Dose', 12);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(301, 'Mühlen Kölsch', '10 L Fässchen', 12);
-INSERT INTO STUDENT.PRODUCT(ID, NAME, DESCRIPTION, PRODUCT_CATEGORY) VALUES(302, 'Gaffel Kölsch', '10 L Fässchen', 12);
-*/
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (2, 'Sinalco Cola', 0.33, 24, 13.50, 0, 0, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (3, 'Fanta', 1, 12, 11.99, 0, 0, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (4, 'Gerolsteiner Mineralwasser', 0.7, 12, 3.49, 0, 0, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (5, 'Franziskaner Weizen', 0.5, 20, 14.99, 16, 4.9, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (6, 'Erdinger Weizen', 0.5, 20, 15.99, 16, 5.1, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (7, 'Bitburger Alkoholfrei', 0.33, 6, 4.99, 0, 0, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (8, 'Propeller Aufwind IPA', 0.33, 6, 14.49, 16, 6.5, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (9, 'CREW Republic Drunken Sailor IPA', 0.33, 20, 28.75, 16, 6.4, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (10, 'CREW Republic 7:45 Esacalation Double IPA', 0.33, 20, 32.00, 16, 8.3, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (11, 'Hess Claritas Kölsch', 0.66, 6, 19.95, 16, 3.9, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (12, 'Mühlen Kölsch', 0.33, 24, 12.69, 16, 4.9, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (13, 'Gaffel Kölsch', 0.5, 20, 13.99, 16, 4.9, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (14, 'Propeller Nachtflug Imperial Stout', 0.33, 6, 12.49, 16, 9.1, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (15, 'Jägermeister', 0.7, 1, 10.49, 18, 35.0, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (16, 'Strothmann Weizenkorn', 0,7, 1, 5.99, 18, 32.0, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (17, 'Kuemmerling Kräuterlikör', 0.02, 25, 11.49, 18, 35.0, null);
+INSERT INTO student.product (id, name, bottle_size_liters, bottles_per_package,  price_per_package, age_restriction, alcohol_percent, product_category) VALUES (18, 'Echter Nordhäuser Doppelkorn', 0.7, 1, 6.89, 18, 38.0, null);
